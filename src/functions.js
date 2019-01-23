667c1b050f52beb57c4656143e3ed0a22a1021fa
blcoccaro <blcoccaro@gmail.com>
2019-01-23 12:16:28 -0200

    deploy automatic

--- src/functions.js
+++ src/functions.js
@@ -228,6 +228,12 @@ export const func = {
                 showConfirmButton: showbutton,
             });
         },
+        randomString() {
+            var S4 = function() {
+               return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+            };
+            return (S4()+S4()+""+S4()+""+S4()+""+S4()+""+S4()+S4()+S4());
+        },
         toastError(message, timer = 4000, showbutton = false) {
             this.$swal({
                 type: 'error',
