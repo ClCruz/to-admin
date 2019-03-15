f4dcafa080170ae8d3839f8e98eeddd93ff275c7
Gabriel Craveiro <gabriel.craveiro@intuiti.com.br>
2019-03-15 14:52:04 -0300

    Testes PWA

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,11 @@ module.exports = {
     configureWebpack: {
       devtool: 'source-map'
     },
-    pwa: {
-      workboxPluginMode: 'InjectManifest',
-      workboxOptions: {
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
-      },
-    },
+    // pwa: {
+    //   workboxPluginMode: 'InjectManifest',
+    //   workboxOptions: {
+    //     swSrc: './src/sw.js',
+    //     swDest: 'service-worker.js',
+    //   },
+    // },
   };
