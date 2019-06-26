0a830db188395c241fe4d7affdc03eba8a63c3fc
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-06-26 09:42:24 -0300

    Filter pie-chart

--- src/views/dashboard/pie-chart.vue
+++ src/views/dashboard/pie-chart.vue
@@ -3,6 +3,22 @@
   <div class="card">
     <div class="card-header">
       <h3 class="card-title">{{title}}</h3>
+      <div class="form-group" v-if="hasFilter">
+        <div class="selectgroup selectgroup-pills">
+          <label class="selectgroup-item">
+            <input type="radio" name="icon-input" value="1" class="selectgroup-input" checked="true" @click="filterAll()">
+              <span class="selectgroup-button selectgroup-button-icon">Todos</span>
+            </label>
+          <label class="selectgroup-item">
+            <input type="radio" name="icon-input" value="2" class="selectgroup-input" @click="filterWeb()">
+              <span class="selectgroup-button selectgroup-button-icon"><i class="fab fa-chrome"></i> Web </span>
+            </label>
+          <label class="selectgroup-item">
+            <input type="radio" name="icon-input" value="3" class="selectgroup-input" @click="filterTicketoffice()">
+              <span class="selectgroup-button selectgroup-button-icon"><i class="fas fa-ticket-alt"></i> Bilheteria</span>
+            </label>
+        </div>
+      </div>
     </div>
     <div class="card-body">
       <div :id="'chart-donut_'+key" class="c3"></div>
@@ -27,24 +43,51 @@ export default {
     }
 
   },
-  methods: {
-
-  },
   props: {
     data: Array,
-    title: String
+    title: String,
+    hasFilter: Boolean
   },
   // [["Disponivel",177],["Vendido",36],["Gratuito",0],["Aguardando pagamento",18],["Reservado",36]]
-
+  methods: {
+    filterAll() {
+      this.chart.load({
+        columns: [
+          this.originalValues.web,
+          this.originalValues.ticketoffice
+        ]
+      });
+    },
+    filterWeb() {
+      this.chart.load({
+        columns: [
+          this.originalValues.web
+        ]
+      });
+      this.chart.load({
+        unload: ['ticketoffice'],
+      });
+    },
+    filterTicketoffice() {
+      this.chart.load({
+        columns: [
+          this.originalValues.ticketoffice
+        ]
+      });
+      this.chart.load({
+        unload: ['web'],
+      });
+    },
+  },
   mounted() {
     c3.generate({
-      bindto: '#chart-donut_'+this.key, // id of chart wrapper
+      bindto: '#chart-donut_' + this.key, // id of chart wrapper
       data: {
         columns: this.data,
         type: 'pie', // default type of chart
       },
       color: {
-        pattern: ['rgb(73,129,204)', 'rgb(244,111,155)', 'rgb(251,150,77)','rgb(55,202,186)',  'rgb(126,208,66)']
+        pattern: ['rgb(73,129,204)', 'rgb(244,111,155)', 'rgb(251,150,77)', 'rgb(55,202,186)', 'rgb(126,208,66)']
       },
       pie: {
         label: {
