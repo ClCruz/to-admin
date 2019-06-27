4c511a30adb9e944716135537fc3f447fdbd7948
Gabriel Craveiro <gabriel.c.craveiro@gmail.com>
2019-06-27 15:51:27 -0300

    Alterações sazarte

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -9,11 +9,11 @@
           </h1>
         </div>
 
-        <b-input-group size="sm" class="col-12 col-sm-12 p-0 pb-4">
+        <b-input-group size="sm" class=" col-12 col-sm-12 p-0 pb-4">
           <b-input-group-prepend is-text class="firstLabel">
             Base:
           </b-input-group-prepend>
-          <b-form-select class="mr-2 col-12 col-md-2" v-on:change="selBase" v-model="form.id_base" :options="selects.base" size="sm">
+          <b-form-select class=" mr-2 col-12 col-md-2" v-on:change="selBase" v-model="form.id_base" :options="selects.base" size="sm">
             <template slot="first">
               <option :value="''" disabled>-- Selecione --</option>
             </template>
@@ -21,7 +21,7 @@
           <b-input-group-prepend is-text class="firstLabel">
             Evento:
           </b-input-group-prepend>
-          <b-form-select class="mr-2 col-6 col-md-6" id="event" v-on:change="selEvent" v-model="form.id_evento" :options="selects.events">
+          <b-form-select class=" mr-2 col-12 col-md-6" id="event" v-on:change="selEvent" v-model="form.id_evento" :options="selects.events">
             <template slot="first">
               <option :value="null" disabled>-- Selecione --</option>
             </template>
@@ -29,7 +29,7 @@
           <b-input-group-prepend is-text class="firstLabel">
             Dia:
           </b-input-group-prepend>
-          <b-form-select class="mr-2 col-6 col-md-1" id="days" :options="selects.days" v-on:change="selDays" v-model="form.date">
+          <b-form-select class=" mr-2 col-12 col-md-1" id="days" :options="selects.days" v-on:change="selDays" v-model="form.date">
             <template slot="first">
               <option :value="null" disabled>-- Selecione --</option>
             </template>
@@ -37,7 +37,7 @@
           <b-input-group-prepend is-text class="firstLabel">
             Horário:
           </b-input-group-prepend>
-          <b-form-select class="mr-2 col-6 col-md-1" id="hours" :options="selects.hours" v-on:change="selHours" v-model="form.hour">
+          <b-form-select class=" mr-2 col-12 col-md-1" id="hours" :options="selects.hours" v-on:change="selHours" v-model="form.hour">
             <template slot="first">
               <option :value="null" disabled>-- Selecione --</option>
             </template>
@@ -714,8 +714,13 @@ export default {
 
   select {
     border-radius: 4px;
-    width: 50%;
+    width: 100%;
     display: flex;
+        position: relative;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    width: 100%;
+    margin-bottom: 0;
   }
   }
 
