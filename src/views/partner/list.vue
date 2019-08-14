952788f34929b95ff70dae477a8089502f1129ae
blcoccaro <blcoccaro@gmail.com>
2019-08-14 17:38:38 -0300

    adicionado tela de forma de pagamento vs parceiro

--- src/views/partner/list.vue
+++ src/views/partner/list.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="mayIsee">
-      <b-modal ref="baseModal" hide-footer title="Usuário x Base">
+      <b-modal ref="baseModal" hide-footer title="Parceiro x Base">
         <div class="d-block text-center">
           <h4>Permissão nas bases para {{popups.base.name}}</h4>
         </div>
@@ -26,6 +26,58 @@
               </template>
         </b-table>
         <b-btn class="mt-3" variant="outline-info" block @click="baseClose">Fechar</b-btn>
+      </b-modal>
+      <b-modal ref="paymenttypeModal" hide-footer title="Parceiro x Forma de Pagamento">
+        <div class="d-block text-center">
+          <h4>Formas de pagamento para {{popups.base.name}}</h4>
+        </div>
+        <b-row class="mb-3">
+          <b-input-group size="sm">
+            <b-input-group-prepend is-text>
+              Base:
+            </b-input-group-prepend>
+            <b-form-select v-on:change="selBase" v-model="form.id_base" :options="selects.base" size="sm" />
+          </b-input-group>
+        </b-row>
+        <b-table striped="striped"
+                  outlined="outlined"
+                  class="fontSize tableClicked bg-white"
+                  small="small"
+                  hover="hover"
+                  responsive
+                  show-empty
+                  empty-text="Não foram encontrados registros."
+                  v-if="this.popups.paymenttype.grids.paymenttype.loaded"
+                  :items="this.popups.paymenttype.grids.paymenttype.items"
+                  :fields="this.popups.paymenttype.grids.paymenttype.fields">
+
+              <template slot="ForPagto" slot-scope="data">
+                  <span v-if="data.item.edit == 0">{{data.item.ForPagto}}</span>
+                  <span v-if="data.item.edit == 1">
+                    <b-input-group size="sm">
+                      <b-form-select v-model="data.item.CodForPagto" :options="selects.forpagto" size="sm" />
+                    </b-input-group>
+                  </span>
+              </template>
+              <template slot="active" slot-scope="data">
+                <b-button size="sm" @click="changePaymenttype('del', data.item)" title="Remover forma de pagamento" variant="danger" v-if="data.item.active == 1">
+                    Remover
+                </b-button>
+
+                <b-button size="sm" @click="changePaymenttype_f1(data.item)" title="Adicionar forma de pagamento" variant="outline-info" v-if="data.item.active != 1 && data.item.edit == 0">
+                    Adicionar
+                </b-button>
+
+                <b-button size="sm" @click="changePaymenttype('add', data.item)" title="Adicionar forma de pagamento" variant="outline-success" v-if="data.item.active != 1 && data.item.edit == 1">
+                    Salvar
+                </b-button>
+                <b-button size="sm" @click="cancelPaymenttype(data.item)" title="Cancelar" variant="outline-warning" v-if="data.item.active != 1 && data.item.edit == 1">
+                    Cancelar
+                </b-button>
+                
+              </template>
+        </b-table>
+        <b-btn class="mt-3" variant="outline-info" block @click="paymenttypeClose">Fechar</b-btn>
       </b-modal>
       <b-container>
         <b-row class="mb-3">
@@ -106,6 +158,7 @@
                   <b-button-group size="sm" v-if="mayI('partner-add', 'partner-wl')">
                       <b-button title="Bases" v-if="mayI('partner-add')" @click.stop="base(data.item,$event.target)">Bases</b-button>
                       <b-button title="Editar" v-if="mayI('partner-add')" @click.stop="edit(data.item,$event.target)">Editar</b-button>
+                      <b-button title="Forma de pagamento" v-if="mayI('partner-paymenttype')" @click.stop="paymenttype(data.item,$event.target)">Forma de Pgto</b-button>
                       <b-button title="Criação whitelabel" variant="danger" v-if="mayI('partner-wl')" @click.stop="whitelabel(data.item,$event.target)">Whitelabel</b-button>
                   </b-button-group>
               </template>
@@ -147,8 +200,61 @@ export default {
     }
   },
   methods: {
+    cancelPaymenttype(item) {
+      item.edit = 0;
+      item.CodForPagto = '';
+    },
+    paymenttypeClose() {
+      this.$refs.paymenttypeModal.hide();
+    },
     baseClose() {
       this.$refs.baseModal.hide();
+    },
+    selBase() {
+      Vue.nextTick().then(response => {
+        this.paymenttype_intern();
+        this.populatePaymenttypeOnBase();
+      });
+    },   
+    paymenttype(item) {
+      this.popups.paymenttype.grids.paymenttype.loaded = false;
+      this.popups.paymenttype.grids.paymenttype.items = [];
+      this.form.id_base = 0;
+
+      this.popups.paymenttype.name = item.name;
+      this.popups.paymenttype.id = item.id;
+      this.$refs.paymenttypeModal.show();
+      this.populateBases(item);
+    },
+    paymenttype_intern() {
+      if (this.processing) return;
+
+      this.popups.paymenttype.grids.paymenttype.processing = true;
+      this.processing = true;
+
+      //this.$wait.start("inprocess");
+      this.showWaitAboveAll();
+      partnerService.listpaymenttype(this.popups.paymenttype.id, this.form.id_base).then(
+        response => {
+          this.processing = false;
+          this.popups.paymenttype.grids.paymenttype.processing = false;
+          this.hideWaitAboveAll();
+          //this.$wait.end("inprocess");
+
+          if (this.validateJSON(response))
+          {
+              this.popups.paymenttype.grids.paymenttype.loaded = true;
+              this.popups.paymenttype.grids.paymenttype.items = response;
+          }
+        },
+        error => {
+          this.popups.paymenttype.grids.paymenttype.processing = false;
+          this.processing = false;
+          this.hideWaitAboveAll();
+          //this.$wait.end("inprocess");
+          this.toastError("Falha na execução.");
+        }
+      );      
     },
     base(item) {
       if (this.processing) return;
@@ -186,6 +292,45 @@ export default {
     },
     refreshBase() {
       this.base({ name: this.popups.base.name, id: this.popups.base.id });
+    },
+    changePaymenttype(type, item) {
+      if (this.processing) return;
+
+      this.processing = true;
+
+      this.showWaitAboveAll();
+      partnerService.savepaymenttype(item.id, this.getLoggedId(), this.form.id_base, item.id_meio_pagamento, item.CodForPagto).then(
+        response => {
+          this.processing = false;
+
+          this.hideWaitAboveAll();
+          //this.$wait.end("inprocess");
+
+          if (this.validateJSON(response))
+          {
+            if (response.success) {
+              if (type == "add")
+                this.toastSuccess("Salvo com sucesso.");
+              else 
+                this.toastSuccess("Removido com sucesso.");
+                
+              this.paymenttype_intern();
+            }
+            else {
+              this.toastError(response.msg);
+            }
+          }
+        },
+        error => {
+          this.processing = false;
+          this.hideWaitAboveAll();
+          //this.$wait.end("inprocess");
+          this.toastError("Falha na execução.");
+        }
+      );      
+    },
+    changePaymenttype_f1(item) {
+      item.edit = item.edit == 0 ? 1 : 0;
     },
     changeBase(type, item) {
       if (this.processing) return;
@@ -235,6 +380,38 @@ export default {
       this.grids.partners.currentPage = to;
       this.search();
     },
+    populateBases() {      
+      this.showWaitAboveAll();
+      partnerService.listbaseselect(this.popups.paymenttype.id).then(
+        response => {
+          this.hideWaitAboveAll();
+
+          if (this.validateJSON(response)) {
+            this.selects.base = response;
+          }
+        },
+        error => {
+          this.hideWaitAboveAll();
+          this.toastError("Falha na execução.");
+        }
+      );
+    },
+    populatePaymenttypeOnBase() {      
+      this.showWaitAboveAll();
+      partnerService.listpaymenttypeonbase(this.form.id_base).then(
+        response => {
+          this.hideWaitAboveAll();
+
+          if (this.validateJSON(response)) {
+            this.selects.forpagto = response;
+          }
+        },
+        error => {
+          this.hideWaitAboveAll();
+          this.toastError("Falha na execução.");
+        }
+      );
+    },
     search() {
       if (this.processing) return;
 
@@ -274,6 +451,12 @@ export default {
         loading: false,
         form: {
           search: '',
+          id_base: '',
+          CodForPagto: '',
+        },
+        selects: {
+          base: [],
+          forpagto: [],
         },
         popups: {
           base: {
@@ -287,6 +470,23 @@ export default {
                 items: [],
                 fields: {
                   ds_nome_teatro: { label: 'Nome', sortable: false },
+                  active: { label: '', sortable: false },
+                },
+              }
+            }
+          },
+          paymenttype: {
+            loaded: false,
+            name: '',
+            id: '',
+            grids: {
+              paymenttype: {
+                processing: false,
+                loaded: false,
+                items: [],
+                fields: {
+                  ds_meio_pagamento: { label: 'Nome', sortable: false },
+                  ForPagto: { label: 'Vinculo na base', sortable: false },
                   active: { label: '', sortable: false },
                 },
               }
