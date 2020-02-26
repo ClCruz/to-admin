307d79dd8537d02fb14e0f52d631d0a71c6388e1
Arthur Rodrigues <arthurrg23@gmail.com>
2020-02-26 13:18:45 -0300

    teste flag pin

--- src/components/common/services/event.js
+++ src/components/common/services/event.js
@@ -7,19 +7,19 @@ Vue.use(VueResource);
 config.setapikey();
 
 export const eventService = {
-  list,
-  get,
-  save,
-  base64,
-  select,
-  selectDays,
-  selectDayHours,
-  selectPresentation,
-  borderokey,
-  borderourl,
-  borderohtml,
-  searchPage,
-  borderolinksend
+    list,
+    get,
+    save,
+    base64,
+    select,
+    selectDays,
+    selectDayHours,
+    selectPresentation,
+    borderokey,
+    borderourl,
+    borderohtml,
+    searchPage,
+    borderolinksend
 }
 
 function borderolinksend(loggedId, id_evento, date, hour, email, pass) {
@@ -36,7 +36,7 @@ function borderolinksend(loggedId, id_evento, date, hour, email, pass) {
                     error: true,
                     msg: err
                 });
-            });    
+            });
         }
     );
     return ret;
@@ -51,7 +51,7 @@ function borderohtml(url) {
     //let url = borderourl();
 
     //let obj = { loggedId, id_evento, date, hour };
-    let obj = { };
+    let obj = {};
 
     var ret = new Promise(
         function (resolve, reject) {
@@ -62,7 +62,7 @@ function borderohtml(url) {
                     error: true,
                     msg: err
                 });
-            });    
+            });
         }
     );
     return ret;
@@ -82,7 +82,7 @@ function borderokey(loggedId, id_evento, date, hour, id_base) {
                     error: true,
                     msg: err
                 });
-            });    
+            });
         }
     );
     return ret;
@@ -102,7 +102,7 @@ function base64(id, type) {
                     error: true,
                     msg: err
                 });
-            });    
+            });
         }
     );
     return ret;
@@ -113,89 +113,89 @@ function get(loggedId, id_evento, id_base) {
     let url = config.api + `/v1/admin/event/get?id_user=${loggedId}&id_evento=${id_evento}&id_base=${id_base}`;
 
     var ret = new Promise(
-    function (resolve, reject) {
-        Vue.http.get(url).then(res => {
-        resolve(res.body);
-        }, err => {
-        reject({
-            error: true,
-            msg: err
-        });
-        });
-    }
-  );
-  return ret;
+        function (resolve, reject) {
+            Vue.http.get(url).then(res => {
+                resolve(res.body);
+            }, err => {
+                reject({
+                    error: true,
+                    msg: err
+                });
+            });
+        }
+    );
+    return ret;
 }
 
 function searchPage(type, input, startAt, howMany, city, state) {
     let url = config.api + `/v1/search/result?input=${input}&type=${type}`;
     var ret = new Promise(
-      function (resolve, reject) {
-        Vue.http.get(url).then(res => {
-          resolve(res.body);
-        }, err => {
-          reject({
-            error: true,
-            msg: err
-          });
-        });
-      }
+        function (resolve, reject) {
+            Vue.http.get(url).then(res => {
+                resolve(res.body);
+            }, err => {
+                reject({
+                    error: true,
+                    msg: err
+                });
+            });
+        }
     );
     return ret;
-  }
+}
 function list(loggedId, search, currentPage, perPage) {
     let url = config.api + `/v1/admin/event/list?loggedId=${loggedId}&search=${search}`;
     url = config.system.applyPagination(url, currentPage, perPage);
 
     var ret = new Promise(
-    function (resolve, reject) {
-        Vue.http.get(url).then(res => {
-        resolve(res.body);
-        }, err => {
-        reject({
-            error: true,
-            msg: err
-        });
-        });
-    }
-  );
-  return ret;
+        function (resolve, reject) {
+            Vue.http.get(url).then(res => {
+                resolve(res.body);
+            }, err => {
+                reject({
+                    error: true,
+                    msg: err
+                });
+            });
+        }
+    );
+    return ret;
 }
 
-function selectPresentation(loggedId,id_evento) {
+function selectPresentation(loggedId, id_evento) {
     let url = config.api + `/v1/admin/event/selectpresentation?loggedId=${loggedId}&id_evento=${id_evento}`;
 
     var ret = new Promise(
-    function (resolve, reject) {
-        Vue.http.get(url).then(res => {
-        resolve(res.body);
-        }, err => {
-        reject({
-            error: true,
-            msg: err
-        });
-        });
-    }
-  );
-  return ret;
+        function (resolve, reject) {
+            Vue.http.get(url).then(res => {
+                resolve(res.body);
+            }, err => {
+                reject({
+                    error: true,
+                    msg: err
+                });
+            });
+        }
+    );
+    return ret;
 }
 
-function select(loggedId,id_base) {
+function select(loggedId, id_base) {
     let url = config.api + `/v1/admin/event/select?loggedId=${loggedId}&id_base=${id_base}`;
 
     var ret = new Promise(
-    function (resolve, reject) {
-        Vue.http.get(url).then(res => {
-        resolve(res.body);
-        }, err => {
-        reject({
-            error: true,
-            msg: err
-        });
-        });
-    }
-  );
-  return ret;
+        function (resolve, reject) {
+            Vue.http.get(url).then(res => {
+                resolve(res.body);
+            }, err => {
+                reject({
+                    error: true,
+                    msg: err
+                });
+            });
+        }
+    );
+    return ret;
 }
 function selectDays(loggedId, id_base, id_evento) {
     let url = config.api + `/v1/admin/event/selectdays?id_base=${id_base}&id_evento=${id_evento}&loggedId=${loggedId}`;
@@ -209,7 +209,7 @@ function selectDays(loggedId, id_base, id_evento) {
                     error: true,
                     msg: err
                 });
-            });    
+            });
         }
     );
     return ret;
@@ -226,41 +226,41 @@ function selectDayHours(loggedId, id_base, id_evento, datePresentation) {
                     error: true,
                     msg: err
                 });
-            });    
+            });
         }
     );
     return ret;
 }
 
-function save(id_to_admin_user, id_base,id_produtor
-    ,CodPeca,NomPeca,CodTipPeca
-    ,TemDurPeca,CenPeca,id_local_evento
-    ,ValIngresso,description,meta_description
-    ,meta_keyword,opening_time,insurance_policy
-    ,showInBanner,bannerDescription,QtIngrPorPedido
-    ,in_obriga_cpf,qt_ingressos_por_cpf,ticketoffice_askemail
-    ,imagechanged,imagebase64,free_installments
-    ,max_installments,interest_rate,ticketoffice_ticketmodel
-    ,showonline,minAmount,maxAmount
-    ,in_entrega_ingresso, external_uri,mmAmountIsPer, qt_hr_anteced, descriptionVoucher, descriptionVoucher2) {
+function save(id_to_admin_user, id_base, id_produtor
+    , CodPeca, NomPeca, CodTipPeca
+    , TemDurPeca, CenPeca, id_local_evento
+    , ValIngresso, description, meta_description
+    , meta_keyword, opening_time, insurance_policy
+    , showInBanner, bannerDescription, QtIngrPorPedido
+    , in_obriga_cpf, qt_ingressos_por_cpf, ticketoffice_askemail
+    , imagechanged, imagebase64, free_installments
+    , max_installments, interest_rate, ticketoffice_ticketmodel
+    , showonline, minAmount, maxAmount
+    , in_entrega_ingresso, showPin, external_uri, mmAmountIsPer, qt_hr_anteced, descriptionVoucher, descriptionVoucher2) {
 
 
 
     let url = config.api + `/v1/admin/event/save`;
 
     let obj = {
-        id_base,id_produtor,id_to_admin_user
-        ,CodPeca,NomPeca,CodTipPeca
-        ,TemDurPeca,CenPeca,id_local_evento
-        ,ValIngresso,description,meta_description
-        ,meta_keyword,opening_time,insurance_policy
-        ,showInBanner,bannerDescription,QtIngrPorPedido
-        ,in_obriga_cpf,qt_ingressos_por_cpf,ticketoffice_askemail
-        ,imagechanged,imagebase64,free_installments
-        ,max_installments,interest_rate,ticketoffice_ticketmodel
-        ,showonline,minAmount,maxAmount
-        ,in_entrega_ingresso,external_uri,mmAmountIsPer
-        ,qt_hr_anteced, descriptionVoucher, descriptionVoucher2
+        id_base, id_produtor, id_to_admin_user
+        , CodPeca, NomPeca, CodTipPeca
+        , TemDurPeca, CenPeca, id_local_evento
+        , ValIngresso, description, meta_description
+        , meta_keyword, opening_time, insurance_policy
+        , showInBanner, bannerDescription, QtIngrPorPedido
+        , in_obriga_cpf, qt_ingressos_por_cpf, ticketoffice_askemail
+        , imagechanged, imagebase64, free_installments
+        , max_installments, interest_rate, ticketoffice_ticketmodel
+        , showonline, minAmount, maxAmount
+        , in_entrega_ingresso, showPin, external_uri, mmAmountIsPer
+        , qt_hr_anteced, descriptionVoucher, descriptionVoucher2
     };
 
 
@@ -269,12 +269,12 @@ function save(id_to_admin_user, id_base,id_produtor
             Vue.http.post(url, obj, { emulateJSON: true }).then(res => {
                 resolve(res.body);
             },
-            (err) => {
-                console.log("Err", err);
-              })
-            .catch((e) => {
-              console.log("Caught", e);
-            })
+                (err) => {
+                    console.log("Err", err);
+                })
+                .catch((e) => {
+                    console.log("Caught", e);
+                })
         }
     );
     return ret;
